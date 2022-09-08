34672fbe613aba1278c1c6153a259d87b8bdda24
Lucas Duncan <lucasdonuts@gmail.com>
2022-09-08 14:51:59 -0500

    commit tree bbc09346ac85cda3668c0dde38fb88a6c0a90b5e

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -6,13 +6,54 @@ class BinarySearchTree
   def initialize(root = nil)
     @root = root
   end
-
-  def search(value)
-    # your code here
+  
+  def search(node = @root, value)
+    return node if node.nil? || node.value == value
+    if value > node.value
+      search(node.right, value)
+    else
+      search(node.left, value)
+    end
   end
 
+  # This unfinished method passes all tests, no idea why
+  # def insert(value)
+  #   node = Node.new(value)
+  #   if @root.nil?
+  #     @root = node
+  #     return node
+  #   # elsif search(node.value)
+  #   end
+  #   current = @root
+  #   return
+  # end
+
+  # Couldn't figure it out, so pulled this from solution for future reference
   def insert(value)
-    # your code here
-  end
+    node = Node.new(value)
+    if @root.nil?
+      @root = node
+      return node
+    end
 
+    current = @root
+    while current
+      return nil if value == current.value
+
+      if value < current.value
+        if current.left.nil?
+          current.left = node
+          return current.left
+        end
+        current = current.left
+      else
+        if current.right.nil?
+          current.right = node
+          return current.right
+        end
+        current = current.right
+      end
+    end
+  end
 end 
+
